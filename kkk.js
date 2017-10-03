f4bea6dc3072fc070934a0455e47c57c6759681b
jessie75919 <jessie75919@gmail.com>
2017-10-03 21:39:15 +0800

    change in SP4

---
+++ kkk.js
@@ -0,0 +1,11 @@
+alert("hello");
+
+
+while (condition) {
+    for (var key in object) {
+        if (object.hasOwnProperty(key)) {
+            var element = object[key];
+            
+        }
+    }
+}
