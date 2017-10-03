52a4b317fefae97415427a52681ccd20b396feed
jessie75919 <jessie75919@gmail.com>
2017-10-03 20:49:05 +0800

    update 2 files

--- abc.js
+++ abc.js
@@ -2,3 +2,8 @@ for (var index = 0; index < array.length; index++) {
     var element = array[index];
     
 }
+
+
+array.forEach(function(element) {
+    
+}, this);
--- ttt.js
+++ ttt.js
@@ -1,1 +1,11 @@
 alert("hello");
+
+
+while (condition) {
+    for (var key in object) {
+        if (object.hasOwnProperty(key)) {
+            var element = object[key];
+            
+        }
+    }
+}
